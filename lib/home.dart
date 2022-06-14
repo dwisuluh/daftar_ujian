4a10d72921fefd68f782e1015167f8224b709a07
dwisuluh <dwi.suluh@students.utdi.ac.id>
2022-06-14 22:35:40 +0700

    update weather

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,162 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'login.dart';
+import 'package:weather/weather.dart';
+
+enum AppState { NOT_DOWNLOADED, DOWNLOADING, FINISHED_DOWNLOADING }
 
 //Class home ketika navigasi home di klik
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _Home createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  String key = '335dfe746c35195c62c6ef074c337766';
+  late WeatherFactory ws;
+  List<Weather> _data = [];
+  AppState _state = AppState.NOT_DOWNLOADED;
+  String? cityName;
+
+  @override
+  void initState() {
+    super.initState();
+    ws = new WeatherFactory(key, language: Language.INDONESIAN);
+  }
+
+  void queryForecast() async {
+    /// Removes keyboard
+    FocusScope.of(context).requestFocus(FocusNode());
+    setState(() {
+      _state = AppState.DOWNLOADING;
+    });
+
+    List<Weather> forecasts = await ws.fiveDayForecastByCityName(cityName!);
+    setState(() {
+      _data = forecasts;
+      _state = AppState.FINISHED_DOWNLOADING;
+    });
+  }
+
+  void queryWeather() async {
+    /// Removes keyboard
+    FocusScope.of(context).requestFocus(FocusNode());
+
+    setState(() {
+      _state = AppState.DOWNLOADING;
+    });
+
+    Weather weather = await ws.currentWeatherByCityName(cityName!);
+    setState(() {
+      _data = [weather];
+      _state = AppState.FINISHED_DOWNLOADING;
+    });
+  }
+
+  Widget contentFinishedDownload() {
+    return Center(
+      child: ListView.separated(
+        itemCount: _data.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_data[index].toString()),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider();
+        },
+      ),
+    );
+  }
+
+  Widget contentDownloading() {
+    return Container(
+      margin: EdgeInsets.all(25),
+      child: Column(children: [
+        Text(
+          'Fetching Weather...',
+          style: TextStyle(fontSize: 20),
+        ),
+        Container(
+            margin: EdgeInsets.only(top: 50),
+            child: Center(child: CircularProgressIndicator(strokeWidth: 10)))
+      ]),
+    );
+  }
+
+  Widget contentNotDownloaded() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Press the button to download the Weather forecast',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _resultView() => _state == AppState.FINISHED_DOWNLOADING
+      ? contentFinishedDownload()
+      : _state == AppState.DOWNLOADING
+          ? contentDownloading()
+          : contentNotDownloaded();
+
+  void _saveLat(String input) {
+    cityName = input;
+    print(cityName);
+  }
+
+  Widget _coordinateInputs() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+              margin: EdgeInsets.all(5),
+              child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Enter latitude'),
+                  keyboardType: TextInputType.text,
+                  onChanged: _saveLat,
+                  onSubmitted: _saveLat)),
+        ),
+      ],
+    );
+  }
+
+  Widget _buttons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(5),
+          child: TextButton(
+            child: Text(
+              'Cek Cuaca',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: queryWeather,
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue)),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(5),
+          child: TextButton(
+            child: Text(
+              'Perkiraan Cuaca',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: queryForecast,
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue)),
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,31 +179,49 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: Card(
-        margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              leading:
-                  Image(image: AssetImage('asset/images/student-icon.jpeg')),
-              title: Text('Dwi Suluh Pribadi'),
-              subtitle: Text('195411132'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                TextButton(
-                  child: const Text('DETAIL'),
-                  onPressed: () {
-                    action(context);
-                  },
+                const ListTile(
+                  leading: Image(
+                      image: AssetImage('asset/images/student-icon.jpeg')),
+                  title: Text('Dwi Suluh Pribadi'),
+                  subtitle: Text('195411132'),
                 ),
-                const SizedBox(width: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text('DETAIL'),
+                      onPressed: () {
+                        action(context);
+                      },
+                    ),
+                    const SizedBox(width: 8),
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
+          ),
+          _coordinateInputs(),
+          _buttons(),
+          Text(
+            "Output : ",
+            style: TextStyle(fontSize: 18),
+          ),
+          Divider(
+            height: 20.0,
+            thickness: 2.0,
+          ),
+          Expanded(
+              child: Card(
+            child: _resultView(),
+          ))
+        ],
       ),
     );
   }
